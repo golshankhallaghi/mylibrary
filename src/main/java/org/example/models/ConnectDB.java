398af19c426c0bae9d114785f5af8b15f59a2c33
Golshan Khallaghi <golshankhallaghi@gmail.com>
2025-07-11 14:53:31 +0330

    linked code to connect to the DB file

--- src/main/java/org/example/models/ConnectDB.java
+++ src/main/java/org/example/models/ConnectDB.java
@@ -1,4 +1,35 @@
 package org.example.models;
 
-public class BonnectDB {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+    private Connection connection;
+
+    public ConnectDB(){
+        String url = "jdbc:sqlite:mylibrary.db";
+        try {
+            connection = DriverManager.getConnection(url);
+            System.out.println("connection established");
+        } catch (SQLException e) {
+            System.out.println("error connecting");
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+
+    }
+    public void closeConnection() {
+        try {
+            if(connection!=null){
+                System.out.println("connection closed");
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
